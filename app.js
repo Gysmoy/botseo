88578f347eed552de1679e5cba1ca20b29540a09
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2023-12-12 12:14:30 -0500

    NTS

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 const puppeteer = require('puppeteer');
 
 const init = async (timeout) => {
-    const browser = await puppeteer.launch({
-        headless: 'new'
-    });
-    const page = await browser.newPage();
-    await page.goto('https://www.google.com');
-    await page.type('textarea[name="q"]', 'sode.me');
-    await page.keyboard.press('Enter');
-    await page.waitForNavigation();
-    await Promise.all([
-        page.click('[data-async-context]#rso a'),
-        page.waitForNavigation(),
-        page.waitForResponse(q =>
-            q.url().includes('sode.me')
-            && q.status() === 200
-        )
-    ])
-    await browser.close();
-    console.log(`--> Searching sode.me\n--> Done\nNext search in ${timeout}ms`)
+    try {
+        const browser = await puppeteer.launch({
+            headless: 'new',
+            // headless: false,
+        });
+        const page = await browser.newPage();
+        await page.goto('https://www.google.com');
+        await page.type('textarea[name="q"]', 'sode.me');
+        await page.keyboard.press('Enter');
+        await page.waitForNavigation();
+        await Promise.all([
+            page.click('[data-async-context^="query"] a'),
+            page.waitForNavigation(),
+            page.waitForResponse(q =>
+                q.url().includes('sode.me')
+                && q.status() === 200
+            )
+        ])
+        await browser.close();
+        console.log(`--> Searching sode.me\n--> Done\nNext search in ${timeout}ms`)
+    } catch (error) {
+        console.log('Error: ', error.message);
+    }
+    
 }
 
 (async () => {
